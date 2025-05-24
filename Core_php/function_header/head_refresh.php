1f7e46504e0bd17f0ca5b2c29fb8489467900fdb
RajeshNagar701 <rajeshnagar.tops@gmail.com>
2025-05-24 11:41:45 +0530

    first commit

---
+++ Core_php/function_header/head_refresh.php
@@ -0,0 +1,14 @@
+
+
+<!--
+
+header("refresh:5;wellcome_location.php");
+ redirect automatic after 5 ms 
+
+-->
+<?php
+
+	echo "you will redirected in 5 second look it";
+	header("refresh:5;wellcome_location.php"); // you will redirected in five second on wellcome_refresh.php page
+?>
+
